d0c70e3e0060e09e9fbcd401d4dab8196799bcdc
Alistair Smith <alistairsmith01@gmail.com>
2020-08-17 03:15:28 +0100

    de-misunderestimating

--- src/client.ts
+++ src/client.ts
@@ -1,10 +1,21 @@
-import { Client } from "discord.js";
+import { Client, Intents } from "discord.js";
 import { log as _log } from "./util/log";
 
 const log = _log("Client");
 
 export const client = new Client({
-  fetchAllMembers: true,
+  messageCacheLifetime: 300,
+  messageCacheMaxSize: 15,
+  messageSweepInterval: 900,
+  partials: ["MESSAGE", "REACTION", "USER"],
+  ws: {
+    intents: [
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.GUILD_MESSAGES,
+      Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+      Intents.FLAGS.GUILD_MEMBERS,
+    ],
+  },
 });
 
 client.on("ready", () => {
